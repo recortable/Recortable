502a201b38d46108dfe55f8c0c039c9596daf101
Danigb <danigb@gmail.com>
2011-02-09 23:22:59 +0100

    Recortable message

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Recortable::Application.config.secret_token = '716d686348a87b06ea7af4a7695718c1063413433673e41007eea437b89f0a815ccf50c79558b03d9c1bbe971a0645762b9f66c377b8bbe0e85d594b8cc1c666'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Recortable::Application.routes.draw do
+  root :to => "welcome#index"
+end
