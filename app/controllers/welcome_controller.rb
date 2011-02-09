01be367efb62a8022e9ac25af3369dfe3db77014
Danigb <danigb@gmail.com>
2011-02-10 00:45:44 +0100

    testing different cache strategies, part 2

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   #http_cache :index, :last_modified => 10.seconds.from_now
   def index
-  	expires_in 1.minute
+  	expires_in 1.minute, :public => true
   	#response.last_modified = 10.seconds.from_now
     #response.headers['Cache-Control'] = 'public, max-age=300'
   end
